92555762565802f60ef93c11ccaf73a35db034de
Andrew Ford <Ford.andrewid@gmail.com>
2014-07-30 02:35:11 -0700

    Fix create deck spec

--- spec/features/create_deck_spec.rb
+++ spec/features/create_deck_spec.rb
@@ -6,8 +6,8 @@ describe 'Creating a new deck' do
     visit new_deck_url
     
     expect(current_path).to eq(sign_up_path)    
-    # expect(page).to have_text('Please sign in first.')
-    # expect(page).not_to have_text("Let's build a deck.")
+    expect(page).to have_text('only registered users can do that.')
+    expect(page).not_to have_text("Let's build a deck.")
   end
 
   it 'is permitted by signed-in users' do
@@ -20,23 +20,21 @@ describe 'Creating a new deck' do
     expect(page).to have_text("Let's build a deck.")
   end
 
-  it 'saves the deck, then shows its details' do 
+  it 'saves the deck, then goes to the edit page' do 
     create_a_user
     sign_in(@user)
 
     visit new_deck_path
     expect(current_path).to eq(new_deck_path)
 
-    fill_in 'Title', with: deck_attributes[:title]
-    fill_in 'Instructions', with: deck_attributes[:instructions]
-    
-    click_button 'Create Deck'
+    fill_in 'deck_title', with: deck_attributes[:title]
+        
+    click_button 'Build It'
 
-    expect(current_path).to eq(deck_path(Deck.last))
+    expect(current_path).to eq(edit_deck_path(Deck.last))
 
     expect(page).to have_text(deck_attributes[:title])
-    expect(page).to have_text(deck_attributes[:name])
-    expect(page).to have_text('Deck created successfully')
+    expect(page).to have_text('Deck built successfully')
   end
 
   it 'does not get saved with a tiny title' do
@@ -45,23 +43,22 @@ describe 'Creating a new deck' do
 
     visit new_deck_path
 
-    fill_in 'Title', with: "XOX"
-    fill_in 'Instructions', with: deck_attributes[:instructions]
+    fill_in 'deck_title', with: "XOX"
 
-    expect {click_button 'Create Deck'}.not_to change(Deck, :count)
-    expect(page).to have_text('error')
+    expect {click_button 'Build It'}.not_to change(Deck, :count)
+    expect(page).to have_text('Please fix')
+    expect(page).to have_text('too short')
   end
 
-  it 'also creates a chapter' do 
+  it 'also creates a first chapter for cards' do 
     create_a_user
     sign_in(@user)
 
     visit new_deck_path
 
-    fill_in 'Title', with: 'Big Deck of Cards'
-    fill_in 'Instructions', with: 'Jokers are wild.'
+    fill_in 'deck_title', with: 'Big Deck of Cards'
   
-    click_button 'Create Deck'
+    click_button 'Build It'
 
     expect(Deck.last.chapters.size).to eq(1)
     expect(page).to have_text('Chapter 1')
