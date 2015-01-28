a324778f5098c822697c09a120ea89bdfbaee15b
Andrew Ford <ford.andrewid@gmail.com>
2015-01-27 23:05:24 -0800

    Update specs and views for button ids

--- spec/features/decks/edit_deck_spec.rb
+++ spec/features/decks/edit_deck_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe "Editing a deck" do
-  it "updates the deck and shows its updated details" do
+  it "updates the deck and shows its updated details", js: true do
     create_user_and_sign_in
     create_deck
 
-    visit edit_deck_url(@deck)
+    visit edit_deck_path(@deck)
+    click_button("deck-settings-toggle-button")
 
     expect(current_path).to eq(edit_deck_path(@deck))
     expect(page).to have_text(deck_attributes[:title])
@@ -15,7 +16,7 @@ describe "Editing a deck" do
 
     expect(current_path).to eq(edit_deck_path(@deck))
     expect(page).to have_text("Deck updated successfully")
-    expect(page).to have_text("Pragmatic Ruby Motion")
+    expect(page).to have_text("Pragmatic")
   end
 
   it 'is not permitted by other users' do
