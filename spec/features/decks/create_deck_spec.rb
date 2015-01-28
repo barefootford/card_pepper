dbd4f5a35687d1e17dbe82434f454ca2c96d92b2
Andrew Ford <ford.andrewid@gmail.com>
2015-01-27 23:04:41 -0800

    refactor create deck spec

--- spec/features/decks/create_deck_spec.rb
+++ spec/features/decks/create_deck_spec.rb
@@ -1,51 +1,53 @@
 require 'spec_helper'
 
-describe 'Creating a new deck' do 
+describe 'Creating a new deck' do
 
-  it 'is not permitted by signed-out users' do 
-    visit new_deck_url
-    
-    expect(current_path).to eq(sign_up_path)    
-    expect(page).to have_text('only registered users can do that.')
-    expect(page).not_to have_text("Let's build a deck.")
+  describe 'while signed out' do 
+
+    it 'is not permitted by signed-out users' do
+      visit new_deck_url
+
+      expect(current_path).to eq(sign_up_path)
+      expect(page).to have_text('only registered users can do that.')
+      expect(page).not_to have_text("Let's build a deck.")
+    end
   end
 
-  it 'is permitted by signed-in users' do
-    create_user
-    sign_in(@user)
+  describe 'while signed in' do 
 
-    visit new_deck_path
-    
-    expect(current_path).to eq(new_deck_path)
-    expect(page).to have_text("Let's build a deck.")
-  end
+    before(:each) do
+      user = create_user
+      sign_in(user)
+    end
 
-  it 'saves the deck, then goes to the edit page' do 
-    create_user
-    sign_in(@user)
+    it 'is permitted by signed-in users' do
+      visit new_deck_path
 
-    visit new_deck_path
-    expect(current_path).to eq(new_deck_path)
+      expect(current_path).to eq(new_deck_path)
+      expect(page).to have_text("Let's build a deck.")
+    end
 
-    fill_in 'deck_title', with: deck_attributes[:title]
-    click_button 'Build It'
+    it 'saves the deck, then goes to the edit page' do
+      visit new_deck_path
+      expect(current_path).to eq(new_deck_path)
 
-    # expect(current_path).to eq(edit_deck_path(Deck.last))
+      fill_in 'deck_title', with: deck_attributes[:title]
+      click_button 'Build It'
 
-    # expect(page).to have_text(deck_attributes[:title])
-    # expect(page).to have_text('Deck built successfully')
-  end
+      expect(current_path).to eq(edit_deck_path(Deck.last))
 
-  it 'does not get saved with a tiny title' do
-    create_user
-    sign_in(@user)
+      expect(page).to have_text(deck_attributes[:title])
+      expect(page).to have_text('Deck built successfully')
+    end
 
-    visit new_deck_path
+    it 'displays validation errors' do
+      visit new_deck_path
 
-    fill_in 'deck_title', with: "XOX"
+      fill_in 'deck_title', with: "XOX"
 
-    expect {click_button 'Build It'}.not_to change(Deck, :count)
-    expect(page).to have_text('Please fix')
-    expect(page).to have_text('too short')
+      expect {click_button 'Build It'}.not_to change(Deck, :count)
+      expect(page).to have_text('Please fix')
+      expect(page).to have_text('too short')
+    end
   end
 end
