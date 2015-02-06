f29f8afa059474bfddd211557eb9829075facb45
Andrew Ford <ford.andrewid@gmail.com>
2015-02-06 01:21:44 -0800

    remove dashboard specs
    Removing dashboard specs… This really shouldn’t be a resource, but
    should just be a partial that can be loaded anywhere.

--- spec/features/dashboard/show_dashboard_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe 'Viewing the dashboard' do 
-  it 'can be viewed while signed in'
-  it 'cannot be viewed while signed out'
-  it 'can only be seen by its owner'
-end
