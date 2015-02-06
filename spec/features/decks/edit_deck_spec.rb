8b6561a118344a6bf6300c5daf32b8daa08d6aab
Andrew Ford <ford.andrewid@gmail.com>
2015-02-06 01:29:50 -0800

    Update editing deck integration specs

--- spec/features/decks/edit_deck_spec.rb
+++ spec/features/decks/edit_deck_spec.rb
@@ -1,30 +1,30 @@
 require 'spec_helper'
 
-describe "Editing a deck" do
-  it "updates the deck and shows its updated details", js: true do
-    create_user_and_sign_in
+describe 'Editing a deck' do
+
+  before do
+    create_user
+    create_second_user
     create_deck
+  end
 
-    visit edit_deck_path(@deck)
-    click_button("deck-settings-toggle-button")
+  describe 'with an owner account' do
+    it 'updates the deck and shows its updated details', js: true do
+      sign_in(@user)
+      visit edit_deck_path(@deck)
 
-    expect(current_path).to eq(edit_deck_path(@deck))
-    expect(page).to have_text(deck_attributes[:title])
+      click_button('deck-settings-toggle-button')
 
-    fill_in 'deck_title', with: 'Pragmatic Ruby Motion'
-    click_button 'save-deck'
+      fill_in 'deck_title', with: deck_attributes[:title]
+      click_button 'save-deck'
 
-    expect(current_path).to eq(edit_deck_path(@deck))
-    expect(page).to have_text("Deck updated successfully")
-    expect(page).to have_text("Pragmatic")
+      expect(current_path).to eq(edit_deck_path(@deck))
+      expect(page).to have_text('Deck updated successfully')
+      expect(page).to have_text('Pragmatic')
+    end
   end
 
   it 'is not permitted by other users' do
-    create_user
-    create_deck
-
-    create_second_user
-
     sign_in(@user2)
     visit edit_deck_path(@deck)
 
