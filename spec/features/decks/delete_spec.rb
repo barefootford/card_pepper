9945f885d1869e779010836096d457c7120f109d
Andrew Ford <ford.andrewid@gmail.com>
2015-02-06 01:53:35 -0800

    update delete specs
    convert ‘current_url’ to ‘current_path’ to satisfy webkit js driver
    gotchas.

--- spec/features/decks/delete_spec.rb
+++ spec/features/decks/delete_spec.rb
@@ -1,31 +1,29 @@
 require 'spec_helper'
 
-describe 'Deleting a deck' do 
-  before do 
+describe 'Deleting a deck' do
+  before do
     create_user
     create_deck
+    create_second_user
   end
 
-  it 'can be done by the decks creator' do 
+  it 'can be done by the decks creator', js: true do
     sign_in(@user)
     visit edit_deck_path(@deck)
-    expect(@user.decks.count).to eq(1)
-    
+    click_button('deck-settings-toggle-button')
+
     click_link('Delete Deck')
 
-    expect(current_url).to eq(root_url)
+    expect(current_path).to eq(root_path)
     expect(@user.decks.count).to eq(0)
-    expect(page).to have_text("Deck deleted successfully.")
+    expect(page).to have_text('Deck deleted successfully.')
   end
 
-  it 'cannot be done someone else' do 
-    @user2 = User.create!(user_attributes(email:"User2@example.com", first_name:'Another', last_name:'User'))
+  it 'cannot be done by the decks non-owner' do
     sign_in(@user2)
-
     visit edit_deck_path(@deck)
-   
+
     expect(current_path).to eq(root_path)
-    expect(page).not_to have_link('Delete Deck')
-    expect(page).to have_text("Only the deck's creator can edit the deck")
+    expect(page).to have_text("Only the deck's creator can edit the deck.")
   end
 end
