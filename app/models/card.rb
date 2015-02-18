bb2cbb92f86d39b89b0887ee0fd5eb9155e0a0aa
Andrew Ford <ford.andrewid@gmail.com>
2015-02-17 23:22:53 -0800

    update model validations, card version methods

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,12 +1,22 @@
 class Card < ActiveRecord::Base
   belongs_to :deck
   belongs_to :user
+
+  has_many :versions
+
   validates :question, :answer, presence: :true, length: { maximum: 140, minimum: 1 }
-  validates :deck_id, presence: :true
+  validates :deck_id, presence: true
+  validates :user_id, presence: true
+
+  after_save :create_version
 
   scope :saved, -> { where('id > 0') }
 
-  def user
-    deck.user
+  def create_version(user_id=self.user_id)
+    self.versions.create(question: self.question, answer: self.answer, user_id: user_id)
+  end
+
+  def latest
+    @latest ||= self.versions.last
   end
 end
