5d1a8ff83af53691e0aa971bf2ccd2d166cc71f4
Andrew Ford <ford.andrewid@gmail.com>
2015-02-19 23:18:29 -0800

    create basic integration specs for creating, approving card suggestions

--- spec/features/card_suggestions/create_spec.rb
+++ spec/features/card_suggestions/create_spec.rb
@@ -1,20 +1,24 @@
 require 'spec_helper'
 
-describe 'Creating a card suggestion' do 
-  
-  # xit 'through the show decks page ' do
-  #   create_user
-  #   create_deck
-  #   create_second_user
-  #   sign_in(@user2)
-  #   visit deck_path(@deck)
+describe 'while signed in' do
+  before(:each) do
+    create_user
+    create_deck
+    sign_in(create_second_user)
+  end
 
-  #   fill_in('new-card-question', with:'Roja')
-  #   fill_in('new-card-answer', with:'Red')
-  #   click_button('save-card-suggestion')
-  
-  #   expect(current_path).to eq(deck_path(@deck))
-    
-  #   expect(page).to have_text('Card suggestion added.') 
-  # end
+  describe 'Creating a card suggestion' do
+    it 'through the show decks page ' do
+      expect(@deck.card_suggestions.count).to eq(0)
+      visit deck_path(@deck)
+
+      fill_in('card-suggestion-question', with:'Roja')
+      fill_in('card-suggestion-answer', with:'Red')
+      click_button('save-card-suggestion')
+      
+      expect(current_path).to eq(deck_path(@deck))
+      expect(page).to have_text('Card suggestion added.') 
+      expect(@deck.card_suggestions.count).to eq(1)
+    end
+  end
 end
