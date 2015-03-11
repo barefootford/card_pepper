d0064f9959dc6e284425a8f0c2c74b0f1c3ebb45
Andrew Ford <ford.andrewid@gmail.com>
2015-03-10 18:15:16 -0700

    add deck subscription routes, clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,23 @@
 Pepper::Application.routes.draw do
 
   root 'decks#index'
-
   get 'beta' => 'pages#beta', as: 'beta'
   get 'anki-import' => 'decks#anki_import', as: 'anki-import'
-
-  #SESSIONS
-  get 'sign_in' => 'sessions#new'
-  get 'sign_up' => 'users#new'
-  
-
-  # Get these outta here newb. 
-  get 'users/:id/delete' => 'users#delete', as: 'user_delete'
-  get 'users/:id/edit_password' => 'users#edit_password', as: 'edit_password'
-
-  get 'session/destroy' => 'sessions#destroy'
-
-  patch 'users/:id/edit_password' => 'users#update_password', as: 'update_password'
-
-  resources :users
-  resource :session
-
   get 'dashboard' => 'dashboards#show', as: 'dashboard'
 
-  resources :decks do 
+  resources :users do
+    resources :deck_subscriptions, only: [:show, :create, :update, :destroy]
+  end
+  get 'users/:id/delete' => 'users#delete', as: 'user_delete'
+  get 'users/:id/edit_password' => 'users#edit_password', as: 'edit_password'
+  patch 'users/:id/edit_password' => 'users#update_password', as: 'update_password'
+
+  resource :session
+  get 'session/destroy' => 'sessions#destroy'
+  get 'sign_in' => 'sessions#new'
+  get 'sign_up' => 'users#new'
+
+  resources :decks do
     resources :cards, only: [ :create, :destroy, :update ]
     resources :card_suggestions, only: [ :create, :update ]
   end
