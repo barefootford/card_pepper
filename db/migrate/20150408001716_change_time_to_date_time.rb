fd9caeb47f7de699d737d1a1433c9f2cfa1c43f8
Andrew Ford <ford.andrewid@gmail.com>
2015-04-07 23:43:52 -0700

    update migration so it doesn't try to convert times to date times

--- db/migrate/20150408001716_change_time_to_date_time.rb
+++ db/migrate/20150408001716_change_time_to_date_time.rb
@@ -1,7 +1,11 @@
 class ChangeTimeToDateTime < ActiveRecord::Migration
   def change
-    change_column(:user_cards, :last_view, :datetime)
-    change_column(:user_cards, :first_view, :datetime)
-    change_column(:user_cards, :next_view, :datetime)
+    remove_column(:user_cards, :last_view)
+    remove_column(:user_cards, :first_view)
+    remove_column(:user_cards, :next_view)
+
+    add_column(:user_cards, :last_view, :datetime)
+    add_column(:user_cards, :first_view, :datetime)
+    add_column(:user_cards, :next_view, :datetime)
   end
 end
