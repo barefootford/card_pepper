9b44e77f2cc81a760505269afa1b3a1b2fd7b35f
Andrew Ford <ford.andrewid@gmail.com>
2016-06-06 02:13:47 +0100

    create a view helpers object for react components

---
+++ app/assets/javascripts/view_helpers.js
@@ -0,0 +1,47 @@
+var ViewHelpers = {
+  communityContributionsCount(cardsArray, deckEditorID) {
+    // Community contributions are any cards where the
+    // card's user_id is not the deck editor's id.
+    // That means the card was created by someone else.
+    return(_.filter(cardsArray, function(card) {
+      return card.user_id !== deckEditorID
+    }).length)
+
+  },
+
+  pluralizeCard: function(number) {
+    if (_.isUndefined(number)) {
+      console.log("pluralizeCard was called without a number.");
+    };
+
+    if ( _.isNumber(number) ) {
+      if (number === 1) {
+        return '1 card'
+      } else if (number > 1) {
+        return (_.toString(number) + ' cards')
+      } else {
+        return '0 cards'
+      }
+    } else {
+      return '0 cards'
+    }
+  },
+
+  pluralizeContrib: function(number) {
+    if (_.isUndefined(number)) {
+      console.log("pluralizeContrib was called without a number.");
+    };
+
+    if ( _.isNumber(number) ) {
+      if (number === 1) {
+        return '1 community contribution'
+      } else if (number > 1) {
+        return (_.toString(number) + ' community contributions')
+      } else {
+        return '0 community contributions'
+      }
+    } else {
+      return '0 community contributions'
+    }
+  }
+};
