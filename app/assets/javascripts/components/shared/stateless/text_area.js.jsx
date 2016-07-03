76d973a730b0a8c451f1800e4c7b89e2a4b256bb
Andrew Ford <ford.andrewid@gmail.com>
2016-07-03 20:01:08 +0100

    create a TextArea component

---
+++ app/assets/javascripts/components/shared/stateless/text_area.js.jsx
@@ -0,0 +1,30 @@
+var TextArea = React.createClass({
+  propTypes: {
+    text: React.PropTypes.string.isRequired,
+    defaultText: React.PropTypes.string,
+    // We don't require onChange so the field can be sorta immutable when saving
+    onChange: React.PropTypes.func,
+    callbackAttribute: React.PropTypes.string,
+    callbackAttributeID: React.PropTypes.number
+  },
+
+  style: {
+    marginBottom: '10px'
+  },
+
+  render: function() {
+    var text = _.toString(this.props.text);
+    var defaultText = _.toString(this.props.defaultText);
+
+    return(
+      <textarea
+        defaultValue={text.length > 0 ? text : defaultText}
+        onChange={this.props.onChange}
+        className='form-control'
+        style={this.style}
+        data-callback-attribute={this.props.callbackAttribute}
+        data-callback-attribute-id={this.props.callbackAttributeID}
+      />
+    )
+  }
+});
